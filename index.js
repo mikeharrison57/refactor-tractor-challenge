3e404cbe20f9459c0b4e854221269a87ebf64709
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-05-27 10:59:49 -0600

    feat: fetching word data from words API

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+
 // Global Variables
 var winningWord = '';
 var currentRow = 1;
 var guess = '';
 var gamesPlayed = [];
+var words;
 
 // Variables associated with playing the game
 
@@ -22,8 +24,24 @@ var gameOverBox = document.querySelector('#game-over-section');
 var gameOverGuessCount = document.querySelector('#game-over-guesses-count');
 var gameOverGuessGrammar = document.querySelector('#game-over-guesses-plural');
 
+// API Calls 
+const fetchWordData = () => {
+  return fetch("http://localhost:3001/api/v1/words")
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.log("Word API error!"));
+};
+
+const fetchData = () => {
+  return Promise.all([fetchWordData()]).then(data => {
+    words = data[0];
+    setGame();
+  })
+};
+
+
 // Event Listeners
-window.addEventListener('load', setGame);
+window.addEventListener('load', fetchData);
 
 // on load the game is set
 
